146d2c8760cbbfeb37d4b050c1ef034c3501be36
creativedude <freddie@creativedude.co.za>
2022-01-24 18:17:14 +0200

    Chore(setup) Initial commit Set up new angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/organisms/list/list.component';
+import { CardComponent } from './components/molecules/card/card.component';
+import { ChipComponent } from './components/atoms/chip/chip.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
